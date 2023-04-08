40586802bde063c60f98fc9ff81001f6e51fe7c4
timakon <1910tim@mail.ru>
2023-04-08 22:46:13 +0300

    edit profile

--- controllers/mongodb/profileController.js
+++ controllers/mongodb/profileController.js
@@ -1,29 +1,18 @@
 const Profile = require('../../models/mongodb/Profile');
 
-// exports.getProfileByUserId = async (req, res) => {
-//   try {
-//     const profile = await Profile.findById(req.params.id);
-//     res.json(profile);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: 'Ошибка сервера' });
-//   }
-// };
 exports.getProfileByUserId = async (req, res) => {
-  console.log(req.params.id);
-    try {
-      const user = await Profile.findById({_id:req.params.id});
-      res.render('user', { user: user });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Ошибка сервера' });
-    }
-  };
+  try {
+    const user = await Profile.findById({_id:req.params.id});
+    console.log(user);
+    res.render('user', { user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Ошибка сервера' });
+  }
+};
   exports.updateProfileByUserId = async (req, res) => {
     const userId = req.params.id;
     const updatedProfileData = req.body;
-
-    console.log(req);
   
     try {
       const updatedProfile = await Profile.findOneAndUpdate(
@@ -31,12 +20,13 @@ exports.getProfileByUserId = async (req, res) => {
         updatedProfileData,
         { new: true, runValidators: true }
       );
-  
+      
       if (!updatedProfile) {
         return res.status(404).json({ error: 'Профиль не найден' });
       }
-  
-      res.status(200).json({ message: 'Профиль успешно обновлен', profile: updatedProfile });
+      const user = await Profile.findById({_id:req.params.id});
+      // res.status(200).json({ message: 'Профиль успешно обновлен', profile: updatedProfile });
+      res.render('user', { user });
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Ошибка сервера' });
--- routes/mongodb/profileRoutes.js
+++ routes/mongodb/profileRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const profileController = require('../../controllers/mongodb/profileController');
 
 router.get('/:id', profileController.getProfileByUserId);
-router.put('/:id', profileController.updateProfileByUserId);
+router.post('/:id', profileController.updateProfileByUserId);
 
 
 module.exports = router;
